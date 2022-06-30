d4b70f86c372673d23c1f2fca929ef1c6a0d8d37
valen-tino <valentinojehaut@gmail.com>
2022-06-30 10:55:00 +0800

    Remove banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Navbar from './sections/nav'
 import Hero from './sections/hero'
-import Banner1 from './sections/banner1'
 import Services from './sections/services'
 
 function App() {
--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -10,14 +10,6 @@ export default function Modal (props) {
         <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
             <div className="relative w-auto max-w-3xl mx-auto my-6 y">
               <div className="relative flex flex-col w-full bg-orange-100 border-0 rounded-lg shadow-lg outline-none focus:outline-none">
-                {/* <div className="flex items-start justify-between p-5 border-b border-orange-200 border-solid rounded-t">
-                  
-                  <button className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none" onClick={hide}>
-                    <span className="block w-6 h-6 ">
-                    <i className="text-teal-900 fa-solid fa-times"></i>
-                    </span>
-                  </button>
-                </div> */}
                 <div className="relative flex-auto p-4">
                   <h3 className="text-3xl font-semibold">{title}</h3><br/>
                   <p className="text-xs leading-relaxed text-black">{desc}</p>
