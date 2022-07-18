8001a3ca0cb4a95e72b67c1f22092c07fc7b4d3d
valen-tino <valentinojehaut@gmail.com>
2022-07-18 16:28:36 +0800

    colors

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -13,7 +13,7 @@ const About = () => {
             <div className='grid bg-orange-200 md:grid-cols-2'>
                 
                 <div className='flex flex-col justify-center w-full md:items-end' data-aos="fade-down">
-                <img src={Vinsen} className='w-full h-full rounded-3xl md:rounded-tr-3xl md:rounded-b-3xl'/>
+                <img src={Vinsen} className='w-full h-full rounded-3xl md:rounded-tr-3xl md:rounded-b-3xl' alt='Vinsensius Jehaut'/>
                 </div>
                 
                 <div className='flex flex-col justify-center w-full px-5 pt-16 pb-6 md:pt-20 md:pl-10 md:items-start'>
