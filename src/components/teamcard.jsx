1aef4400baee2207d1b39ff8e99d2f4f37dc0bef
valen-tino <valentinojehaut@gmail.com>
2022-07-18 09:57:44 +0800

    tes fiks 3

--- src/components/teamcard.jsx
+++ src/components/teamcard.jsx
@@ -4,7 +4,7 @@ export default function teamCard (props) {
     const name = props.name
     const title = props.title
     const desc = props.desc
-    const path = "../src/"+props.path; 
+    const path = "src/"+props.path; 
   
     return (
     <>
